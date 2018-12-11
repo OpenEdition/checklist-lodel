d0326e899a74cc9fbcb536be7dc6a7d8735c165f
Thomas Brouard <contact@brrd.fr>
2018-12-11 15:25:45 +0100

    Affichage des grands tableaux (largetable)

--- js/checklist-tpl.js
+++ js/checklist-tpl.js
@@ -1,7 +1,10 @@
-// Ajouter des ancres aux paragraphes
 $(function () {
+  // Ajouter des ancres aux paragraphes
   $(".ckl-field-text > .ckl-field-value > p.texte").each(function (index) {
     var num = index + 1;
     $(this).attr("id", "p" + num);
   });
+
+  // Largetable
+  $(".ckl-field-text table").largetable();
 });
