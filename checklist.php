c2f8a3f601d3191844423831f1b3cbe55ac0ae90
Thomas Brouard <contact@brrd.fr>
2019-03-19 16:57:27 +0100

    Add "Checklist" tab in Lodel desk

--- checklist.php
+++ checklist.php
@@ -10,12 +10,31 @@ class checklist extends Plugins {
 	}
 
 	public function postview (&$context) {
-		if(!defined('backoffice-admin') || C::get('do') || C::get('lo') || ($context['view']['tpl'] != 'index')) return;
+		$displayOnTpl = array("article", "sommaire", "entrees", "personnes");
+		$tpl = $context['view']['tpl'];
+
+		if(defined('backoffice-admin') || C::get('do') || C::get('lo') || !in_array($tpl, $displayOnTpl)) return;
 		if(!parent::_checkRights(LEVEL_REDACTOR)) return;
 
-		$pattern = '/Plugins<\/a><\/li>/';
-		$replacement = '$0<li><a href="./?do=_checklist_view">Checklist</a></li>';
-		View::$page = preg_replace($pattern, $replacement, View::$page);
+		// Get page contents
+		$page = View::$page;
+		$dom = new DOMDocument;
+		$dom->loadHTML($page);
+		$finder = new DomXPath($dom);
+		$ul = $finder->query('//*[@id="lodel-desk"]//*[contains(concat(" ",normalize-space(@class)," ")," group1 ")]')[0];
+
+		// Create a "Checklist" item in Lodel tabs
+		$li = $dom->createElement('li');
+		$ul->appendChild($li);
+		$a = $dom->createElement('a', 'Checklist');
+		$href = $dom->createAttribute('href');
+		$href->value = './?do=_checklist_view&amp;document=' . $context['id'];
+		$a->appendChild($href);
+		$li->appendChild($a);
+
+		// Update page contents
+		$page = $dom->saveHTML();
+		View::$page = $page;
 	}
 
 	public function viewAction (&$context, &$error) {
