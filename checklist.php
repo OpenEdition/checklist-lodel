5466c965efb6d3ab3258321eacaf3d51edd00a0d
Thomas Brouard <contact@brrd.fr>
2019-03-19 15:42:57 +0100

    Use checklist template in plugin

--- checklist.php
+++ checklist.php
@@ -4,19 +4,22 @@ class checklist extends Plugins {
 
 	public function disableAction (&$context, &$error) {}
 
-	public function preview (&$context) {}
+	public function preview (&$context)	{
+		if(!defined('backoffice') || $context['view']['tpl'] != 'checklist') return;
+		C::set('view.base_rep.checklist', 'checklist');
+	}
 
 	public function postview (&$context) {
 		if(!defined('backoffice-admin') || C::get('do') || C::get('lo') || ($context['view']['tpl'] != 'index')) return;
 		if(!parent::_checkRights(LEVEL_REDACTOR)) return;
 
 		$pattern = '/Plugins<\/a><\/li>/';
-		$replacement = '$0<li><a href="./?do=_checklist_test">Checklist</a></li>';
+		$replacement = '$0<li><a href="./?do=_checklist_view">Checklist</a></li>';
 		View::$page = preg_replace($pattern, $replacement, View::$page);
 	}
 
-	public function testAction (&$context, &$error) {
-		echo "Hello Checklist" ;
+	public function viewAction (&$context, &$error) {
+		View::getView()->renderCached('checklist') ;
 		return "_ajax" ;
 	}
 }
