c24353c70008f317144e95aecae5873b98ed6d14
Thomas Brouard <contact@brrd.fr>
2020-03-03 11:16:31 +0100

    Disable numbering on illustrations (fix #65)

--- public/js/checklist-lodel.js
+++ public/js/checklist-lodel.js
@@ -1,6 +1,12 @@
 $(function () {
-  // Ajouter des ancres aux paragraphes
+  // Ne pas numéroter les images + ajouter des ancres aux paragraphes
   $(".ckl-field-text > .ckl-field-value > p.texte").each(function (index) {
+    var isImage = $(this).find("img").length === 1 && $(this).text().trim() === "";
+    if (isImage) {
+      $(this).attr("data-numbering", "false");
+      return;
+    }
+
     var num = index + 1;
     $(this).attr("id", "p" + num);
   });
