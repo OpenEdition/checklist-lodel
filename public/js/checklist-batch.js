782bf1f81de1b0245eabd6262c2162c345607b62
Thomas Brouard <contact@brrd.fr>
2021-03-19 12:41:05 +0100

    Add options to export form

--- public/js/checklist-batch.js
+++ public/js/checklist-batch.js
@@ -1,3 +1,6 @@
+// Page init
+// =========
+
 function initTagify($input) {
 	if (Tagify == null) {
 		console.error("Tagify not found");
@@ -15,6 +18,38 @@ function initTagify($input) {
 		}
 	});
 }
+
+function initOptions() {
+	var cache = checklist.ui.cache;
+	var isPaperActive = !cache.getFilter("tag-paper");
+	if (isPaperActive) {
+		$("#display-paper").prop( "checked", true );
+	}
+}
+
+// User input getters
+// ==================
+
+function getInputVal($input) {
+	var inputVal = $input.val();
+	if (inputVal == null || inputVal.trim() === "") {
+		inputVal = "0";
+	}
+	return inputVal;
+}
+
+function getOptions() {
+	var options = {};
+	$(".ckl-batch-option input").each(function() {
+		var name = $(this).attr("name");
+		var val = $(this).prop("checked");
+		options[name] = val;
+	});
+	return options;
+}
+
+// CSV Export
+// ==========
 
 function getDocById(docs, id) {
 	return docs.find(function(doc) {
@@ -41,6 +76,88 @@ function downloadCSV(filename, data) {
 	}
 }
 
+function runBatch(ids, options) {
+	$.ajax({
+		url : "?do=_checklist_get",
+		type : "GET",
+		data: "documents=" + ids,
+		dataType : 'json'
+	})
+	.done(function(data, textStatus) {
+		if (data.length === 0) {
+			alert("Cette publication est introuvable.");
+			return;
+		}
+
+		var docs = data.map(function(doc) {
+			return {
+				docId: doc.id,
+				href: doc.href,
+				context: doc.context
+			}
+		});
+		checklist.runBatch({ docs: docs }).then(function(checkers) {
+			var statements = checkers.reduce(function(res, checker) {
+				if (options.displayError && checker.error) {
+					res.push(checker);
+					return res;
+				}
+
+				var checkerStatements = checker.getStatements();
+
+				if (options.displayPaper === false) {
+					checkerStatements = checkerStatements.filter(function(s) {
+						return s.tags.includes("paper") === false;
+					});
+				}
+
+				if (options.displayOk && checkerStatements.length === 0) {
+					res.push({ docId: checker.docId, ok: true });
+					return res;
+				}
+
+				return res.concat(checkerStatements);
+			}, []);
+
+			if (statements.length === 0) {
+				alert("Cette requête a produit un fichier CSV vide.");
+				return;
+			}
+
+			var colHeaders = '"Publication", "Document", "Type", "État", "Message", "Tag", "Total" \r\n';
+
+			var table = statements.reduce(function(res, s) {
+				var id = s.docId;
+				var doc = getDocById(data, id);
+				var eol = "\r\n";
+
+				// Error
+				if (s.error) {
+					var errMsg = s.error.message;
+					return res + '"' + doc.idpubli + '","' + id + '","' + doc.type + '","error","' + errMsg + '",,' + eol;
+				}
+
+				// OK = checker with no statement
+				if (s.ok) {
+					return res + '"' + doc.idpubli + '","' + id + '","' + doc.type + '","ok",,,' + eol;
+				}
+				
+				// Statement
+				var tk = checklist.ui.tk;
+				var tags = s.tags.join(", ");
+				return res + '"' + doc.idpubli + '","' + id + '","' + doc.type + '","' + s.type + '","' + tk(s.name) + '","' + tags + '","' + s.count + '"' + eol;
+			}, colHeaders);
+
+			var filename = "checklist-" + ids.replace(/\D+/g, "-") + ".csv";
+			downloadCSV(filename, table);
+		});
+	})
+	.fail(console.error);
+}
+
+// Main
+// ====
+
 $(function() {
 	if (window.checklist == null) {
 		console.error("checklist not found");
@@ -49,82 +166,11 @@ $(function() {
 
 	var $input = $("#ckl-batch-input");
 	initTagify($input);
-
-	var tk = checklist.ui.tk;
+	initOptions();
 
 	$("#ckl-batch-btn").on("click", function() {
-		var inputVal = $input.val();
-		if (inputVal == null || inputVal.trim() === "") {
-			inputVal = "0";
-		}
-		$.ajax({
-			url : "?do=_checklist_get",
-			type : "GET",
-			data: "documents=" + inputVal,
-			dataType : 'json'
-		})
-		.done(function(data, textStatus) {
-			if (data.length === 0) {
-				alert("Cette publication est introuvable.");
-				return;
-			}
-
-			var docs = data.map(function(doc) {
-				return {
-					docId: doc.id,
-					href: doc.href,
-					context: doc.context
-				}
-			});
-			checklist.runBatch({ docs: docs }).then(function(checkers) {
-				var statements = checkers.reduce(function(res, checker) {
-					if (checker.error) {
-						res.push(checker);
-						return res;
-					}
-
-					var checkerStatements = checker.getStatements();
-
-					if (checkerStatements.length === 0) {
-						res.push({ docId: checker.docId, ok: true });
-						return res;
-					}
-
-					return res.concat(checkerStatements);
-				}, []);
-
-				if (statements.length === 0) {
-					alert("Cette requête a produit un fichier CSV vide.");
-					return;
-				}
-
-				var colHeaders = '"Publication", "Document", "Type", "État", "Message", "Tag", "Total" \r\n';
-
-				var table = statements.reduce(function(res, s) {
-					var id = s.docId;
-					var doc = getDocById(data, id);
-					var eol = "\r\n";
-
-					// Error
-					if (s.error) {
-						var errMsg = s.error.message;
-						return res + '"' + doc.idpubli + '","' + id + '","' + doc.type + '","error","' + errMsg + '",,' + eol;
-					}
-
-					// Success = checker with no statement
-					if (s.ok) {
-						return res + '"' + doc.idpubli + '","' + id + '","' + doc.type + '","ok",,,' + eol;
-					}
-					
-					// Statement
-					var tags = s.tags.join(", ");
-					return res + '"' + doc.idpubli + '","' + id + '","' + doc.type + '","' + s.type + '","' + tk(s.name) + '","' + tags + '","' + s.count + '"' + eol;
-				}, colHeaders);
-
-				var filename = "checklist-" + inputVal.replace(/\D+/g, "-") + ".csv";
-				downloadCSV(filename, table);
-			});
-		})
-		.fail(console.error);
+		var ids = getInputVal($input);
+		var options = getOptions();
+		runBatch(ids, options);
 	});
 });
