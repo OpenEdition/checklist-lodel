e7ab36d84f68d80d9525a0749ee8ad4c94101781
Thomas Brouard <contact@brrd.fr>
2021-05-06 11:44:59 +0200

    Add issue filter reset button

--- public/js/site.js
+++ public/js/site.js
@@ -15,9 +15,18 @@ $(function () {
   $(".ckl-field-text table").largetable({ enableMaximize: true });
 
   // Filtres de la home
+  var homeUrl = location.protocol + "//" + location.host + location.pathname + "?do=_checklist_view&document=0";
   var $selects = $(".ckl-issues-filter");
+  var $clearBtn = $(".ckl-issue-filters-clear");
+
+  $selects.each(function() {
+    if (this.value === "") return;
+    $clearBtn.removeClass("hidden");
+    return false;
+  });
+
   $selects.on("change", function() {
-    var url = location.protocol + "//" + location.host + location.pathname + "?do=_checklist_view&document=0";
+    var url = homeUrl;
     $selects.each(function() {
       var key = this.dataset.filter;
       var value = this.value;
@@ -25,5 +34,9 @@ $(function () {
       url = url + "&filter_" + key + "=" + value;
     });
     window.location.href = url;
+  });
+
+  $clearBtn.on("click", function() {
+    window.location.href = homeUrl;
   });
 });
